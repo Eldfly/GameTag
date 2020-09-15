655b6e5740956e7b74aa47f50de8a2cbf10d0dc2
Sofia Martinsson <fia.martinsson@gmail.com>
2020-09-15 13:05:23 +0200

    changed url breadcrumb, added title to all sites

--- forums/urls.py
+++ forums/urls.py
@@ -10,49 +10,47 @@ urlpatterns = [
     path('', views.ForumListView.as_view(), name='index'),
 
     #url for list all forums (only used for sitemap)
-    path('forums/<slug:forum_slug>/', views.ForumListView.as_view(), name='forums'),
+    path('forum/<slug:forum_slug>/', views.ForumListView.as_view(), name='forums'),
 
     #path('forums/user_forums/', views.user_forums, name='user_forums'),
 
     #url for creating a new forum
-    path('forums/create_forum/', views.create_forum, name='create_forum'),
+    path('user_forums/create_forum/', views.create_forum, name='create_forum'),
 
     #url for edit a forum
-    path('forums/user_forums/<slug:slug>/edit_forum/', views.ForumUpdateView.as_view(), name='edit_forum'),
+    path('user_forums/<slug:slug>/edit_forum/', views.ForumUpdateView.as_view(), name='edit_forum'),
 
     #url for delete a forum
-    path('forums/user_forums/<slug:slug>/delete_forum/', views.ForumDeleteView.as_view(), name='delete_forum'),
+    path('user_forums/<slug:slug>/delete_forum/', views.ForumDeleteView.as_view(), name='delete_forum'),
 
 
     #url for showing all topics in a forum
-    path('forum/<slug:forum_slug>/', views.TopicListView.as_view(), name='forum_topic'),
+    path('forum/<slug:forum_slug>/topics/', views.TopicListView.as_view(), name='forum_topic'),
 
     #url for creating a new topic in a forum
-    path('forum/<slug:forum_slug>/new_topic/', views.new_topic, name='new_topic'),
+    path('user_forums/<slug:forum_slug>/new_topic/', views.new_topic, name='new_topic'),
 
     #url for edit a topic
-    path('forums/user_forums/<int:topic_id>/edit_topic/', views.TopicUpdateView.as_view(), name='edit_topic'),
+    path('user_forums/<int:topic_id>/edit_topic/', views.TopicUpdateView.as_view(), name='edit_topic'),
 
     #url for delete a topic
-    path('forums/user_forums/<int:pk>/delete_topic/', views.TopicDeleteView.as_view(), name='delete_topic'),
-
-
+    path('user_forums/<int:pk>/delete_topic/', views.TopicDeleteView.as_view(), name='delete_topic'),
 
 
     #url for showing all threads in a forum topic
-    path('forum/<slug:forum_slug>/topic/<int:topic_id>/threads/', views.ThreadListView.as_view(), name='topic_threads'),
+    path('forum/<slug:forum_slug>/topics/<int:topic_id>/threads/', views.ThreadListView.as_view(), name='topic_threads'),
 
     #url for creating a new thread in a forum topic
-    path('forum/<slug:forum_slug>/topic/<int:topic_id>/new_thread/', views.new_thread, name='new_thread'),
+    path('forum/<slug:forum_slug>/topics/<int:topic_id>/new_thread/', views.new_thread, name='new_thread'),
 
     #url for delete a thread
-    path('forum/<slug:forum_slug>/topic/<int:topic_id>/threads/<int:pk>/delete_thread/', views.ThreadDeleteView.as_view(), name='delete_thread'),
+    path('forum/<slug:forum_slug>/topics/<int:topic_id>/threads/<int:pk>/delete_thread/', views.ThreadDeleteView.as_view(), name='delete_thread'),
 
-    #url for showing all posts for a thread in a specific fourm
-    path('forum/<slug:forum_slug>/topic/<int:topic_id>/threads/<int:thread_id>/', views.PostListView.as_view(), name='thread_posts'),
+    #url for showing all posts for a thread in a specific forum
+    path('forum/<slug:forum_slug>/topics/<int:topic_id>/threads/<int:thread_id>/', views.PostListView.as_view(), name='thread_posts'),
 
     #url for reply to a post in a thread
-    path('forum/<slug:forum_slug>/topic/<int:topic_id>/threads/<int:thread_id>/reply/', views.reply_thread, name='reply_thread'),
+    path('forum/<slug:forum_slug>/topics/<int:topic_id>/threads/<int:thread_id>/reply/', views.reply_thread, name='reply_thread'),
 
 
 
